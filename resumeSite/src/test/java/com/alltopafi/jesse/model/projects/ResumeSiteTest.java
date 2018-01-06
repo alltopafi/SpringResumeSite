9b31a3de361e3f669e12d0758beb6b720052b200
alltopafi <alltopafi@gmail.com>
2018-01-06 17:21:55 -0600

    test update

--- resumeSite/src/test/java/com/alltopafi/jesse/model/projects/ResumeSiteTest.java
+++ resumeSite/src/test/java/com/alltopafi/jesse/model/projects/ResumeSiteTest.java
@@ -1,24 +1,34 @@
 package com.alltopafi.jesse.model.projects;
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
 public class ResumeSiteTest {
 
-	@Mock
 	private ResumeSite resumeSite;
 	
 	@Before
 	public void setup() {
-		resumeSite = mock(ResumeSite.class);
+		resumeSite = new ResumeSite();;
 	}
 	
 	@Test
 	public void test() {
-		assertNotNull(resumeSite);
+		resumeSite.setClassName("Networking");
+		resumeSite.setDescription("network stuff");
+		resumeSite.setGrade(80);
+		resumeSite.setName("thing");
+		resumeSite.setTeamMembers(new ArrayList<String>());
+		resumeSite.getTeamMembers().add("Sara");
+		
+		assertEquals("Networking", resumeSite.getClassName());
+		assertEquals("network stuff", resumeSite.getDescription());
+		assertEquals(80, resumeSite.getGrade());
+		assertEquals("thing", resumeSite.getName());
+		assertEquals("Sara", resumeSite.getTeamMembers().get(0));
 	}
 }
